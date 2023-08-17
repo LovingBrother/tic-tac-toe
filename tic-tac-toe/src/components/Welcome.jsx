bb1a39efc9bd6f1bf5a07cd3dd8c3470b454a720
LovingBrother <anaha.sj@gmail.com>
2023-08-17 12:28:48 +0100

    Added Welcome screen

---
+++ tic-tac-toe/src/components/Welcome.jsx
@@ -0,0 +1,10 @@
+import img1 from "../assets/game-background.jpg";
+
+function WelcomePage() {
+  return (
+    <div className="welcome-page">
+      <img src={img1} alt="Welcome" />
+    </div>
+  );
+}
+export default WelcomePage
