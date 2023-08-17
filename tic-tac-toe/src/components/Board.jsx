fbbde747dad89f20612fe1cc7b7ff187634505fe
Benny-Nwaro <baroh3767@aol.com>
2023-08-17 16:09:49 +0100

    finishing touches

--- tic-tac-toe/src/components/Board.jsx
+++ tic-tac-toe/src/components/Board.jsx
@@ -79,8 +79,7 @@ const Board = () => {
       ) : (
         ""
       )}
-      <p><a href="https://github.com/Benny-Nwaro">Aroh Ebenezer</a> | <a href="https://github.com/kingalingaling">King Eshiebor</a> | 
-        <a href="https://github.com/LovingBrother">Jonah Samuel</a></p>
+      <p><a  href="https://github.com/Benny-Nwaro">Aroh Ebenezer</a> | <a href="https://github.com/kingalingaling">King Eshiebor</a> | <a href="https://github.com/LovingBrother">Jonah Samuel</a> </p>
     </div>
   );
 };
