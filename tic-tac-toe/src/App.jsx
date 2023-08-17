bf80af4c9a36ce586f9e886ca0d284431c150c39
Benny-Nwaro <baroh3767@aol.com>
2023-08-17 11:04:21 +0100

    Added squares component

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -1,10 +1,28 @@
 import './App.css'
+import Squares from './components/Squares'
 
 function App() {
   return (
-    <div className=''>
-
+    <>
+    <div className= "game-title">
+      <h3> Tic Tac Toe</h3>
     </div>
+    <div className="board-row">
+        <Squares />
+        <Squares />
+        <Squares />
+    </div>
+    <div className="board-row">
+        <Squares />
+        <Squares />
+        <Squares />
+    </div>
+    <div className="board-row">
+        <Squares />
+        <Squares />
+        <Squares />
+    </div>
+  </>
   )
 }
 
---
+++ tic-tac-toe/src/components/Squares.jsx
@@ -0,0 +1,7 @@
+const Squares = () => {
+  return (
+    <button className="square">1</button>
+  )
+}
+
+export default Squares
